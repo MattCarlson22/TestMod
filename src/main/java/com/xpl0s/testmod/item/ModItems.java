8ad3f930b9aa00c58e9d150d9356071295ad4f1b
Matt Carlson <MattCarlson22@protonmail.com>
2021-12-03 19:05:40 -0700

    Tomatos

--- src/main/java/com/xpl0s/testmod/item/ModItems.java
+++ src/main/java/com/xpl0s/testmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.xpl0s.testmod.item;
 
 import com.xpl0s.testmod.TestMod;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -20,6 +21,10 @@ public class ModItems {
 
     public static final RegistryObject<Item> RAW_TITANIUM = ITEMS.register("raw_titanium",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
+
+    public static final RegistryObject<Item> TOMATO = ITEMS.register("tomato",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)
+                    .food(new FoodProperties.Builder().nutrition(2).saturationMod(0.2f).build())));
 
 
     public static void register(IEventBus eventBus) {
